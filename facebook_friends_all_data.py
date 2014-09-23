005a2ec0b38a2e5ae4a2c9fcc0efc8132081a46a
Abhishek <abhishek_simhadri@yahoo.co.in>
2014-09-23 11:59:05 +0530

    Update facebook_friends_all_data.py

--- facebook_friends_all_data.py
+++ facebook_friends_all_data.py
@@ -1,7 +1,7 @@
 import facebook
 import json
 
-ACCESS_TOKEN = 'CAACEdEose0cBABP4qW5jzcIZBdD688CcObQ5FhdkbSIOdmA56snZA5NB6MMQd0W72BTmezcokZAFcNZCORkvSRD6c0TzWoHbJuSpymREqFlbpxeJePVSqnWVZCuPFVzJmv040ZAPqpiXDUpVovsejwGcFUIgjTwr5TUEpxQ5WftNZBxmDpUQG68CgGjOqk2kUJm3qtn99sGXbW4E11KIIykqEcZBGaej4V8ZD'
+ACCESS_TOKEN = ''
 
 def pp(o):
     print json.dumps(o, indent=1)
